86621c51adad7e6a7db661bcbc40bd3db75d182e
giacomomusic <giacomodmessina@icloud.com>
2026-04-07 11:54:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const supabase = window.supabase.createClient(
   'YOUR_SUPABASE_URL',
   'YOUR_SUPABASE_ANON_KEY'
+
+  async function login() {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  })
+
+  if (error) {
+    alert(error.message)
+  } else {
+    alert('Logged in!')
+  }
+}
